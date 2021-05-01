4e47b0da74c3c65294bd2a5ab5819953b7441f4a
mcgillij <mcgillivray.jason@gmail.com>
2021-04-30 21:05:41 -0300

    more main scene stiched together

--- src/scenes/mainscene.py
+++ src/scenes/mainscene.py
@@ -1,7 +1,6 @@
 """ Main Scene """
 import pygame
 from .scene import Scene
-import time
 import random
 
 import pymunk
@@ -12,10 +11,10 @@ from ..utils import load_image
 
 # from bullet import Bullet
 from ..dorf import Dorf
-from ..managers import InputManager
 
 COLLISION_TYPES = {'ball': 1}
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+pymunk.pygame_util.positive_y_is_up = True
 
 
 class MainScene(Scene):
@@ -26,8 +25,48 @@ class MainScene(Scene):
         self.persist["screen_color"] = 'black'
         self.inputmanager = None
         self.next_scene = "OTHER"
+        self.dorf_images = ["dorf.png", "dorf1.png", "dorf2.png"]
+        self.loaded_images = [load_image(image) for image in self.dorf_images]
 
         print("I'm in main")
+        # pymunk physics
+        self.space = pymunk.Space()
+        self.draw_options = pymunk.pygame_util.DrawOptions(pygame.display.get_surface())
+
+        static_lines = [
+            pymunk.Segment(self.space.static_body, (50, 50), (50, 550), 2),
+            pymunk.Segment(self.space.static_body, (50, 550), (550, 550), 2),
+            pymunk.Segment(self.space.static_body, (550, 550), (550, 50), 2),
+            pymunk.Segment(self.space.static_body, (50, 50), (550, 50), 2),
+        ]
+        for line in static_lines:
+            line.color = pygame.Color('lightgray')
+            line.elasticity = 1.0
+
+        self.space.add(*static_lines)
+
+        dorf_body = pymunk.Body(500, float('inf'))
+        dorf_body.position = Vec2d(0, 0)
+
+        size = 10
+
+        dorf_box = [(-size, -size), (-size, size), (size, size), (size, -size)]
+
+        dorf_shape = pymunk.Poly(dorf_body, dorf_box)
+        #    dorf_shape.position = Vec2d(0, 0)
+        dorf_shape.collision_type = COLLISION_TYPES['ball']
+
+        self.dorf = Dorf(self.loaded_images, dorf_body)
+        # Add dorf to simulation
+        self.space.add(dorf_body, dorf_shape)
+
+        # bullet_image = load_image(
+        #     "bullet.png"
+        # )  # this probably needs to be only loaded once
+
+        # setup sprite group for drawing
+        self.group = pygame.sprite.Group()
+        self.group.add(self.dorf)
 
     def get_event(self, event):
         self.interaction_phase()
@@ -38,21 +77,67 @@ class MainScene(Scene):
             self.done = True
 
     def draw(self, surface):
-        surface.fill(pygame.Color(self.persist['screen_color']))
+        surface.fill((0, 0, 0))
+        # surface.fill(pygame.Color(self.persist['screen_color']))
+        self.group.update()
+        self.group.draw(surface)
+        # do physics with pymunk here
+        self.space.debug_draw(self.draw_options)
+
         surface.blit(self.title, self.title_rect)
 
     def update(self, dt):
-        pass
+        self.process_physics_bullets()
+        self.space.step(dt)
+        # clock.tick(fps)
+        # maintain frame rate
+        # difference = start - time.time()
+        # delay = 1.0 / fps - difference
+        # if delay > 0:
+#            time.sleep(delay)
+
+    def process_physics_bullets(self):
+        if self.dorf.shooting:
+            bullet_body, bullet_shape = self.shoot_a_ball((self.dorf.rect.centerx, flipy(self.dorf.rect.centery, WINDOW_HEIGHT)), random.choice([(1, 10), (-1, 10)]))
+            self.space.add(bullet_body, bullet_shape)
 
     def interaction_phase(self):
         # actions
         for event in self.inputmanager.get_events():
             if event.key == "A" and event.down:
                 print("You pressed A")
+                self.dorf.shooting = True
             if event.key == "A" and event.up:
                 print("You let go of A")
+                self.dorf.shooting = False
             if event.key == "X" and event.up:
                 self.inputmanager.quit_attempted = True
+        # movement
+        if self.inputmanager.is_pressed("left"):
+            self.dorf.move_left()
+        elif self.inputmanager.is_pressed("right"):
+            self.dorf.move_right()
+        if self.inputmanager.is_pressed("up"):
+            self.dorf.move_up()
+        elif self.inputmanager.is_pressed("down"):
+            self.dorf.move_down()
+
+    def shoot_a_ball(self, position, direction):
+        ball_body = pymunk.Body(1, float("inf"))
+        ball_body.position = position
+
+        ball_shape = pymunk.Circle(ball_body, 5)
+        ball_shape.color = pygame.Color('Green')
+        ball_shape.elasticity = 1.0
+        ball_shape.collision_type = COLLISION_TYPES['ball']
+
+        ball_body.apply_impulse_at_local_point(Vec2d(*direction))
+
+        def constant_velocity(body, gravity, damping, dt):
+            body.velocity = body.velocity.normalized() * 400
+
+        ball_body.velocity_func = constant_velocity
+        return ball_body, ball_shape
 
 
 def flipy(pymunk_y, window_height):
@@ -60,137 +145,22 @@ def flipy(pymunk_y, window_height):
     return int(-pymunk_y + window_height)
 
 
-def shoot_a_ball(space, position, direction):
-    ball_body = pymunk.Body(1, float("inf"))
-    ball_body.position = position
-
-    ball_shape = pymunk.Circle(ball_body, 5)
-    ball_shape.color = pygame.Color('Green')
-    ball_shape.elasticity = 1.0
-    ball_shape.collision_type = COLLISION_TYPES['ball']
-
-    ball_body.apply_impulse_at_local_point(Vec2d(*direction))
-
-    def constant_velocity(body, gravity, damping, dt):
-        body.velocity = body.velocity.normalized() * 400
-
-    ball_body.velocity_func = constant_velocity
-    space.add(ball_body, ball_shape)
+# def should_go_in_draw_or_update():
+#            interaction_phase(dorf, input_manager)
+#            # process_bullets(dorf, bullet_image, group)
+#            process_physics_bullets(dorf, space)
+#            # Draw phase
+#            # Draw the dorf
+#
+#        pygame.display.flip()
+#        clock.tick(fps)
+#        # maintain frame rate
+#        difference = start - time.time()
+#        delay = 1.0 / fps - difference
+#        if delay > 0:
+#            time.sleep(delay)
 
 
-def old_main():
-    """ Main game loop """
-
-    # pymunk physics
-    space = pymunk.Space()
-    pymunk.pygame_util.positive_y_is_up = True
-    draw_options = pymunk.pygame_util.DrawOptions(screen)
-
-    static_lines = [
-        pymunk.Segment(space.static_body, (50, 50), (50, 550), 2),
-        pymunk.Segment(space.static_body, (50, 550), (550, 550), 2),
-        pymunk.Segment(space.static_body, (550, 550), (550, 50), 2),
-        pymunk.Segment(space.static_body, (50, 50), (550, 50), 2),
-    ]
-    for line in static_lines:
-        line.color = pygame.Color('lightgray')
-        line.elasticity = 1.0
-
-    space.add(*static_lines)
-
-    input_manager = InputManager()
-
-    button_index = 0
-
-    dorf_images = ["dorf.png", "dorf1.png", "dorf2.png"]
-    loaded_images = [load_image(image) for image in dorf_images]
-
-    dorf_body = pymunk.Body(500, float('inf'))
-    dorf_body.position = Vec2d(0, 0)
-
-    size = 10
-
-    dorf_box = [(-size, -size), (-size, size), (size, size), (size, -size)]
-
-    dorf_shape = pymunk.Poly(dorf_body, dorf_box)
-#    dorf_shape.position = Vec2d(0, 0)
-    dorf_shape.collision_type = COLLISION_TYPES['ball']
-
-    dorf = Dorf(loaded_images, dorf_body)
-    # Add dorf to simulation
-    space.add(dorf_body, dorf_shape)
-
-    # bullet_image = load_image(
-    #     "bullet.png"
-    # )  # this probably needs to be only loaded once
-
-    # setup sprite group for drawing
-    group = pygame.sprite.Group()
-    group.add(dorf)
-
-    # The main game loop
-    while not input_manager.quit_attempt:
-        start = time.time()
-
-        screen.fill((0, 0, 0))
-        # There will be two phases to our "game".
-        is_configured = button_index >= len(input_manager.buttons)
-        # In the first phase, the user will be prompted to configure the joystick by pressing
-        if not is_configured:
-            success = configure_phase(
-                screen, input_manager.buttons[button_index], input_manager
-            )
-            # if the user pressed a button and configured it...
-            if success:
-                # move on to the next button that needs to be configured
-                button_index += 1
-        else:
-            interaction_phase(dorf, input_manager)
-            # process_bullets(dorf, bullet_image, group)
-            process_physics_bullets(dorf, space)
-            # Draw phase
-            # Draw the dorf
-            group.update()
-            group.draw(screen)
-            # do physics with pymunk here
-            space.debug_draw(draw_options)
-            space.step(dt)
-
-        pygame.display.flip()
-        clock.tick(fps)
-        # maintain frame rate
-        difference = start - time.time()
-        delay = 1.0 / fps - difference
-        if delay > 0:
-            time.sleep(delay)
-
-
-def process_physics_bullets(dorf, space):
-    if dorf.shooting:
-        shoot_a_ball(space, (dorf.rect.centerx, flipy(dorf.rect.centery, WINDOW_HEIGHT)), random.choice([(1, 10), (-1, 10)]))
 # def process_bullets(dorf, bullet_image, group):
-        # new_bullet = Bullet(bullet_image, pygame.Rect(dorf.rect.x, dorf.rect.y, 10, 10))
-        # group.add(new_bullet)
-
-
-def interaction_phase_old(dorf, input_manager):
-    # actions
-    for event in input_manager.get_events():
-        if event.key == "A" and event.down:
-            print("You pressed A")
-            dorf.shooting = True
-        if event.key == "A" and event.up:
-            print("You let go of A")
-            dorf.shooting = False
-        if event.key == "X" and event.up:
-            input_manager.quit_attempted = True
-    # movement
-    if input_manager.is_pressed("left"):
-        dorf.move_left()
-    elif input_manager.is_pressed("right"):
-        dorf.move_right()
-    if input_manager.is_pressed("up"):
-        dorf.move_up()
-    elif input_manager.is_pressed("down"):
-        dorf.move_down()
-
+    # new_bullet = Bullet(bullet_image, pygame.Rect(dorf.rect.x, dorf.rect.y, 10, 10))
+    # group.add(new_bullet)
